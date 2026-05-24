444a45e60335b673fb467a658ebeade3d5f35157
Fahad1bashir2 <fahad85581@gmail.com>
2026-05-24 18:00:50 +0500

    update CSS

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
-// import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-
-// export default defineConfig({
-//   plugins: [tailwindcss(), react()],
-// });
-
-import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react' 
-
+// https://vite.dev/config/
 export default defineConfig({
    plugins: [tailwindcss(), react()],
-  // eslint-disable-next-line no-dupe-keys
-  plugins: [react()],
-  optimizeDeps: {
-    disabled: true, // Vite ko automatic optimize karne se rokay ga
-    noDiscovery: true // Naye packages ki auto-discovery band karega
-  }
+  
 })
